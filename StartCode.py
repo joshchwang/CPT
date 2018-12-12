487a7c5660c3eecfdaf38af67900eafb582daff6
FTadros <43042263+FTadros@users.noreply.github.com>
2018-12-12 13:57:24 -0500

    Update StartCode.py

--- StartCode.py
+++ StartCode.py
@@ -1,15 +1,21 @@
 def setup():
     size (1200,600)
-x=0
+playerx = 20
+playery = 300
+vel = 20
+playerw = 100
+playerh = 100
+
 def keyPressed():
-    global x
-    if key == 'a':
-        x -= 4
+    global playerx, playery
+    if key == 'a' and playerx > vel:
+        playerx -= vel
     if key == 'w':
-        pass #jump
-    if key == 'd':
-        x += 4 
+        pass
+    if key == 'd' and playerx < 1200 - playerw - vel:
+        playerx += vel 
+
 def draw():
     background(255)
-    rect(x,300,100,100)
+    rect(playerx, playery, playerw,playerh)
     rect(-10,400,1500,200)
