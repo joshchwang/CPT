819a04d517e8fc98e0823f478326584a197e6ae8
FTadros <43042263+FTadros@users.noreply.github.com>
2018-12-13 16:54:02 -0500

    JUMP WORKS PERFECTLY

--- StartCode.py
+++ StartCode.py
@@ -11,33 +11,32 @@ Right = False
 
 def setup():
     size(1200, 600)          
-            
-
              
 def draw():
+    global playerx, playery, Right, Left, isJump, jumpCounter
     
     if mousePressed:
-        global isJump, jumpCounter
-        if isJump == True and jumpCounter >= -10:
-            neg = 1
-            if jumpCounter < 0 :
-                neg = -1
-            playery -= jumpCounter **2 *.5 * neg
-            jumpCounter-=1
-        else:
-            isJump = False
-            jumpCounter = 10
-            
-        global playerx, playery, Right, Left
-        if mouseX >  600:
-            Right = True   
-        if mouseX < 600:
-            Left = True
-        if Left == True:
-            playerx -= vel
-        if Right == True:
-            playerx += vel
+        isJump = True
+    if isJump == True and jumpCounter >= -10:
+        neg = 1
+        if jumpCounter < 0 :
+            neg = -1
+        playery -= jumpCounter **2 *.5 * neg
+        jumpCounter-=1
+    else:
+        isJump = False
+        jumpCounter = 10
+        
+    if mouseX >  600:
+        Right = True   
+    if mouseX < 600:
+        Left = True
+    if Left == True:
+        playerx -= vel
+    if Right == True:
+        playerx += vel
+    
     print mouseX
     background(255)
-    rect(mouseX, playery, playerw, playerh)
+    rect(mouseX-50, playery, playerw, playerh)
     rect(-10, 400, 1500, 200)
