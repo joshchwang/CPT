724d4353c139381a55b608a771a2e28e4605c9e1
FTadros <43042263+FTadros@users.noreply.github.com>
2018-12-13 12:22:47 -0500

    StartCode+Jump.py
    Jumping physics now mostly working, need to solve movement mid-air

--- StartCode.py
+++ StartCode.py
@@ -16,28 +16,24 @@ def keyPressed():
         playerx -= vel
     if key == 'd' and playerx < 1200 - playerw - vel:
         playerx += vel
+
     if not(isJump):
         if key == 'w':
-            pass
-        if key == 's':
-            pass
-        if key == 'r':
-            isJump = True
-    else:
-        if jumpCount >= -10:
-            neg = 1
-            if jumpCount < 0:
-                neg = -1
-            playery -= jumpCount **2 * .5 * neg
-            jumpCount -= 1
-        else:
-            isJump = False
-            jumpCount = 10
-    print key
-            
+            isJump = True            
         
     
 def draw():
+    global playerx, playery, isJump, jumpCount
+    if isJump == True and jumpCount >= -10:
+        neg = 1
+        if jumpCount < 0 :
+            neg = -1
+        playery -= jumpCount **2 *.5 * neg
+        jumpCount-=1
+    else:
+        isJump = False
+        jumpCount = 10
+        
     background(255)
     rect(playerx, playery, playerw, playerh)
     rect(-10, 400, 1500, 200)
