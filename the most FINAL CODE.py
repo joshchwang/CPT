ad45e8701fe117c6b008f72c8e3f82aa0cd4f4a2
joshchwang <43012087+joshchwang@users.noreply.github.com>
2019-01-17 13:46:21 -0500

    Update the most FINAL CODE.py

--- the most FINAL CODE.py
+++ the most FINAL CODE.py
@@ -18,30 +18,66 @@ collisiony = 200
 enemyy = 500
 platx = 600
 platy = 500
-trianglex = 1360
+trianglex = 1600
 triangley = 600
-sidescrol = 0.7
+sidescrol = 1
 textx = 275
+
 
 def setup():
     size(1360, 700)
-
+    global playerimage, deathlist
+    death2 = loadImage("2.gif")
+    death3 = loadImage("3.gif")
+    death4 = loadImage("4.gif")
+    death5 = loadImage("5.gif")
+    death6 = loadImage("6.gif")
+    death7 = loadImage("7.gif")
+    death8 = loadImage("8.gif")
+    death9 = loadImage("9.gif")
+    death10 = loadImage("10.gif")
+    death11 = loadImage("11.gif")
+    death12 = loadImage("12.gif")
+    death13 = loadImage("13.gif")
+    death14 = loadImage("14.gif")
+    death15 = loadImage("15.gif")
+    death16 = loadImage("16.gif")
+    death17 = loadImage("17.gif")
+    death18 = loadImage("18.gif")
+    death19 = loadImage("19.gif")
+    death20 = loadImage("20.gif")
+    death21 = loadImage("21.gif")
+    death22 = loadImage("22.gif")
+    death23 = loadImage("23.gif")
+    death24 = loadImage("24.gif")
+    death25 = loadImage("25.gif")
+    death26 = loadImage("26.gif")
+    death27 = loadImage("27.gif")
+    death28 = loadImage("28.gif")
+    death29 = loadImage("29.gif")
+    death30 = loadImage("30.gif")
+    deathlist = [ death2, death3, death4, death5, death6, death7, death8, death9, death10,
+                death11, death12, death13, death14, death15, death16, death17, death18, death19,
+                death20, death21, death22, death23, death24, death25, death26, death27, death28,
+                death29, death30 ]
+    playerimage = death2
 
 def draw():
-    global playerx, playerw, playery, Right, Left, isJump
-    global jumpCounter, enemyx, coinlistx, coinlisty, textx
-    global deathcounter, death, collisiony, keys_pressed, platx, trianglex
+    global playerx, playerw, playery, Right, Left, isJump, trianglex
+    global jumpCounter, enemyx, coinlistx, coinlisty, deathlist
+    global deathcounter, death, collisiony, keys_pressed, platx
+    global textx
     background(135, 206, 250)
     noStroke()
     fill('#228B22')
     textSize(50)
     text("It's always so hard to do the right thing,", textx - 75, 200)
     text("But so easy to do the wrong thing.", textx, 250)
-    textx -= sidescrol
+    textx -=  sidescrol
     if not(playery - collisiony == enemyy-1 and
             playerx + playerw > enemyx and
             playerx - playerw < enemyx ):  # redo
-        enemyx -= 1
+        enemyx -= sidescrol + 1
         if keys_pressed[87]:  # w
             pass
         if keys_pressed[32]:  # space
@@ -54,7 +90,7 @@ def draw():
             playerx += vel
     else:
         death = True
-        deathcounter += 1
+        deathcounter += .3
         print 'collided'
 
     if isJump and jumpCounter >= -10:
@@ -108,12 +144,11 @@ def draw():
         #plat6
     elif (playerx >= platx-100+1960 and playerx <= platx+100+1960 and 
         playery - collisiony < platy-1  or 
-        playerx >= platx-100+19d60 and playerx <= platx+100+1960
+        playerx >= platx-100+1960 and playerx <= platx+100+1960
         and playery - collisiony < platy-100-120 and isJump or isJump):
         collisiony = 420
     else:
         collisiony = 200
-
     # Player
     rect(playerx, playery - collisiony, playerw, playerh)
     
@@ -126,9 +161,15 @@ def draw():
     rect(platx + 1960, platy - 120, 100, 10)
     trianglex -= sidescrol
     # Death Code
-    if death:
-        pass
-        # play gif and restart
+    if death == True:
+        if deathcounter >= 28:
+            deathcounter = 0        
+        #Put in picture as failsafe
+        for d in (deathlist):
+            playerimage = deathlist[int(deathcounter)]
+            image(playerimage, width/2-148, height/2-133)
+            if deathcounter == 28:
+                deathcounter = 0
     print playerx, playery - collisiony, enemyy-1
     platx -= sidescrol
 
